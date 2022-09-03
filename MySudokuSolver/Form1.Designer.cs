4e87b4f52e237963be44927742a8531ff4713f70
chanka1021 <achraf.chouach@gmail.com>
2022-09-03 23:34:25 +0100

    Add project files.

---
+++ MySudokuSolver/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace MySudokuSolver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::MySudokuSolver.Properties.Resources.my_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(296, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.AllowUserToOrderColumns = true;
+            this.DataGridView1.AllowUserToResizeColumns = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Khaki;
+            this.DataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.DataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.ColumnHeadersVisible = false;
+            this.DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Crimson;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Khaki;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.DataGridView1.GridColor = System.Drawing.Color.Black;
+            this.DataGridView1.Location = new System.Drawing.Point(351, 34);
+            this.DataGridView1.MultiSelect = false;
+            this.DataGridView1.Name = "DataGridView1";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("NFS font", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.DataGridView1.RowHeadersVisible = false;
+            this.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.DataGridView1.RowTemplate.Height = 43;
+            this.DataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataGridView1.Size = new System.Drawing.Size(426, 390);
+            this.DataGridView1.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MaxInputLength = 1;
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.Width = 47;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MaxInputLength = 1;
+            this.Column2.Name = "Column2";
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.Width = 47;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.MaxInputLength = 1;
+            this.Column3.Name = "Column3";
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.Width = 47;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.MaxInputLength = 1;
+            this.Column4.Name = "Column4";
+            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column4.Width = 47;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.MaxInputLength = 1;
+            this.Column5.Name = "Column5";
+            this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column5.Width = 47;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.MaxInputLength = 1;
+            this.Column6.Name = "Column6";
+            this.Column6.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column6.Width = 47;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.MaxInputLength = 1;
+            this.Column7.Name = "Column7";
+            this.Column7.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column7.Width = 47;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.MaxInputLength = 1;
+            this.Column8.Name = "Column8";
+            this.Column8.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column8.Width = 47;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.MaxInputLength = 1;
+            this.Column9.Name = "Column9";
+            this.Column9.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column9.Width = 47;
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.BackColor = System.Drawing.Color.Black;
+            this.btnSolve.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSolve.Font = new System.Drawing.Font("NFS font", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSolve.ForeColor = System.Drawing.Color.White;
+            this.btnSolve.Location = new System.Drawing.Point(56, 302);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(197, 58);
+            this.btnSolve.TabIndex = 7;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = false;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click_1);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Black;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("NFS font", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(56, 366);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(197, 58);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.DataGridView1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MdiChildrenMinimizedAnchorBottom = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnSolve;
+        private Button btnClear;
+        internal DataGridView DataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
+        private DataGridViewTextBoxColumn Column8;
+        private DataGridViewTextBoxColumn Column9;
+    }
+}
---
+++ MySudokuSolver/Form1.cs
@@ -0,0 +1,160 @@
+using System.Windows.Forms;
+
+namespace MySudokuSolver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int[,] SudokuMatrix = new int[9, 9];
+
+     
+        //Validate and assemble Array based on Grid values
+        private bool ValidGrid()
+        {
+            int num;
+            for (int i = 0; i < DataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < DataGridView1.ColumnCount; j++)
+                {
+
+                    if (DataGridView1.Rows[i].Cells[j].Value == null || DataGridView1.Rows[i].Cells[j].Value == "")
+                    {
+                        SudokuMatrix[i, j] = 0;
+                    }
+                    else if (Int32.TryParse(DataGridView1.Rows[i].Cells[j].Value.ToString(), out num))
+                    {
+                        if (!ValidNum(i, j, num)) return false;
+                        else SudokuMatrix[i, j] = num;
+                    }
+                    else return false;
+
+                }
+            }
+            return true;
+        }
+        private bool Solver(int r, int c)
+        {
+
+            int column = c, row = r;
+
+            //Stop condition
+            if (c == 9) return true;
+            //Get next position
+            if (row < 8) row++;
+            else
+            {
+                row = 0;
+                column++;
+            }
+
+            //Check if the  position is empty
+            if (SudokuMatrix[r, c] != 0) return Solver(row, column);
+            else
+            {
+                //Loop in the possible elements to be filled
+                for (int num = 1; num < 10; num++)
+                {
+                    //Call function to validate if element is valid
+                    if (ValidNum(r, c, num))
+                    {
+                        SudokuMatrix[r, c] = num;
+                        DataGridView1.Rows[r].Cells[c].Value = num;
+                        DataGridView1.Rows[r].Cells[c].Style.ForeColor = Color.Black;
+                       
+                        //Call function with next position
+                        if (Solver(row, column)) return true;
+                        //If it is not possible to fill in the box, reset the value to try again
+                        SudokuMatrix[r, c] = 0;
+                        DataGridView1.Rows[r].Cells[c].Value = "";
+                    }
+                }
+            }
+            return false;
+        }
+        //Returns if a given number is valid 
+        private bool ValidNum(int r, int c, int num)
+        {
+            //Check if it repeats in rows and columns
+            for (int i = 0; i < 9; i++)
+            {
+                if (SudokuMatrix[r, i] == num)
+                    return false;
+                if (SudokuMatrix[i, c] == num)
+                    return false;
+            }
+
+            //Get the quadrant of the number
+            int row = (r / 3) * 3;
+            int column = (c / 3) * 3;
+
+            //Check if it repeats in the quadrant
+            for (int j = row; j < row + 3; j++)
+            {
+                for (int k = column; k < column + 3; k++)
+                {
+                    if (SudokuMatrix[j, k] == num) return false;
+                }
+            }
+
+            return true;
+        }
+        //Clear all Grid cells
+        private void ClearGrid()
+        {
+            for (int i = 0; i < DataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < DataGridView1.ColumnCount; j++)
+                {
+                    DataGridView1.Rows[i].Cells[j].Value = null;
+                    DataGridView1.Rows[i].Cells[j].Style.ForeColor = Color.FromArgb(64, 64, 64);
+                }
+            }
+        }
+
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+            ClearGrid();
+
+        }
+        // Grid Fill
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataGridView1.Rows.Add(9);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    DataGridView1.Rows[i].Cells[j].Style.BackColor = Color.LightGoldenrodYellow;
+                    DataGridView1.Rows[i].Cells[j + 6].Style.BackColor = Color.LightGoldenrodYellow;
+                    DataGridView1.Rows[i + 3].Cells[j + 3].Style.BackColor = Color.LightGoldenrodYellow;
+                    DataGridView1.Rows[i + 6].Cells[j].Style.BackColor = Color.LightGoldenrodYellow;
+                    DataGridView1.Rows[i + 6].Cells[j + 6].Style.BackColor = Color.LightGoldenrodYellow;
+                }
+            }
+        }
+
+        private void btnSolve_Click_1(object sender, EventArgs e)
+        {
+            //Array Clear
+            SudokuMatrix = new int[9, 9];
+            //Validate the grid information
+            if (!ValidGrid())
+            {
+                MessageBox.Show("Invalid", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Call function to solve Sudoku
+            if (Solver(0, 0)) MessageBox.Show("Sudoku solved successfully!", "successfully!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else MessageBox.Show("Impossible to solve Sudoku!", "Alert!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //Grid lock
+            DataGridView1.ReadOnly = true;
+
+        }
+    }
+}
