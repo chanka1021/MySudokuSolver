1a81247723f2ef35e38957c7a788694eaaea93dd
chanka1021 <achraf.chouach@gmail.com>
2022-09-03 23:43:53 +0100

    commit

--- MySudokuSolver/Form1.Designer.cs
+++ MySudokuSolver/Form1.Designer.cs
@@ -222,7 +222,7 @@
             this.MaximizeBox = false;
             this.MdiChildrenMinimizedAnchorBottom = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "MySudoku Solver";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
